44a1f2e1126cf67a9b0653a68e54f53ebd718128
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2025-04-09 18:16:09 +0000

    Generation QrCode

--- src/Services/Gestion.php
+++ src/Services/Gestion.php
@@ -5,7 +5,11 @@ namespace App\Services;
 use App\Entity\Ticket;
 use App\Repository\TicketRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use phpDocumentor\Reflection\Types\Self_;
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\Writer\PngWriter;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class Gestion
 {
@@ -13,11 +17,16 @@ class Gestion
     const STATUT_INVITE = "INVITE";
     const FLAG_INSTALLE = 1;
     const FLAG_NONINSTALLE = 0;
+
+    private string $qrCodeDirector;
     public function __construct(
         private EntityManagerInterface $entityManager,
         private TicketRepository $ticketRepository,
+        string $qrCodeDirectory,
+        private UrlGeneratorInterface $urlGenerator,
     )
     {
+        $this->qrCodeDirector = $qrCodeDirectory;
     }
 
     public function saveParticipation(object $participant)
@@ -27,11 +36,13 @@ class Gestion
 
         if ( $nb_place>=1){
             for ($i = 0; $i < $nb_place; $i++) {
+                $code = $this->codeTicket();
                 $ticket = new Ticket();
                 $ticket->setTelephone($participant->getTelephone());
                 $i === 0 ? $ticket->setStatut(self::STATUT_MEMBRE) : $ticket->setStatut(self::STATUT_INVITE);
                 $ticket->setFlag(self::FLAG_NONINSTALLE);
-                $ticket->setCode($this->codeTicket());
+                $ticket->setCode($code);
+                $ticket->setMedia($this->qrCodeGenerator($code));
 
                 $participant->addTicket($ticket);
 
@@ -50,5 +61,26 @@ class Gestion
         } while($this->ticketRepository->findOneBy(['code' => $matricule]));
 
         return $matricule;
+    }
+
+    public function qrCodeGenerator(string $code): string
+    {
+        $url = $this->urlGenerator->generate('app_home', ['code'=> $code], UrlGeneratorInterface::ABSOLUTE_URL);
+        $builder = new Builder(
+            writer : new PngWriter(),
+            data: $url,
+            encoding: new Encoding('UTF-8'),
+            errorCorrectionLevel: ErrorCorrectionLevel::High,
+            size: 500,
+            margin: 10,
+        );
+        $result = $builder->build();
+
+        $filename = $code . '.png';
+        $path = $this->qrCodeDirector . '/' . $filename;
+
+        $result->saveToFile($path);
+
+        return $filename;
     }
 }
