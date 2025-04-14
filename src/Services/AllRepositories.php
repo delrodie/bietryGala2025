12c4efbb442eab6b8accaea75162e8b35b0a18ff
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2025-04-14 17:06:54 +0000

    Security Login

--- src/Services/AllRepositories.php
+++ src/Services/AllRepositories.php
@@ -4,12 +4,16 @@ namespace App\Services;
 
 use App\Repository\ParticipantRepository;
 use App\Repository\TicketRepository;
+use App\Repository\UserRepository;
 
 class AllRepositories
 {
+
+
     public function __construct(
         private ParticipantRepository $participantRepository,
-        private TicketRepository $ticketRepository
+        private TicketRepository $ticketRepository,
+        private UserRepository $userRepository,
     )
     {
     }
@@ -32,5 +36,30 @@ class AllRepositories
     public function getAllTickets()
     {
         return $this->ticketRepository->findAllTicket();
+    }
+
+    public function getUsers(string $username): array
+    {
+        $getUsers = $this->userRepository->findWithout(['username' => $username]);
+        $users = [];
+
+        foreach ($getUsers as $getUser) {
+            $roles = $getUser->getRoles()[0] ?? $getUser->getRoles();
+            $role = match ($roles) {
+                'ROLE_ADMIN' => 'Administrateur',
+                'ROLE_SUPER_ADMIN' => 'Super Administrateur',
+                default => 'Utilisateur'
+            };
+
+            $users[] = [
+                'id' => $getUser->getId(),
+                'username' => $getUser->getUserIdentifier(),
+                'role' => $role,
+                'connexion' => $getUser->getConnexion(),
+                'lastConnectedAt' => $getUser->getLastConnectedAt(),
+            ];
+        }
+
+        return $users;
     }
 }
